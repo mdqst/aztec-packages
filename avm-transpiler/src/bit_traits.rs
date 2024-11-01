bc3b5163d176e33b99af25256adf09aa73fb68d4
fcarreiro <facundo@aztecprotocol.com>
2024-11-01 11:03:33 +0000

    trying our external PR

--- avm-transpiler/src/bit_traits.rs
+++ avm-transpiler/src/bit_traits.rs
@@ -1,13 +1,11 @@
 use acvm::{acir::brillig::MemoryAddress, AcirField, FieldElement};
 
 fn get_msb(n: u128) -> usize {
-    let mut n = n;
-    let mut msb = 0;
-    while n > 0 {
-        n >>= 1;
-        msb += 1;
+    if n == 0 {
+        0
+    } else {
+        128 - n.leading_zeros() as usize
     }
-    msb
 }
 
 pub trait BitsQueryable {
@@ -20,42 +18,6 @@ impl BitsQueryable for FieldElement {
     }
 }
 
-impl BitsQueryable for u8 {
-    fn num_bits(&self) -> usize {
-        get_msb(*self as u128)
-    }
-}
-
-impl BitsQueryable for u16 {
-    fn num_bits(&self) -> usize {
-        get_msb(*self as u128)
-    }
-}
-
-impl BitsQueryable for u32 {
-    fn num_bits(&self) -> usize {
-        get_msb(*self as u128)
-    }
-}
-
-impl BitsQueryable for u64 {
-    fn num_bits(&self) -> usize {
-        get_msb(*self as u128)
-    }
-}
-
-impl BitsQueryable for u128 {
-    fn num_bits(&self) -> usize {
-        get_msb(*self)
-    }
-}
-
-impl BitsQueryable for usize {
-    fn num_bits(&self) -> usize {
-        get_msb(*self as u128)
-    }
-}
-
 impl BitsQueryable for MemoryAddress {
     fn num_bits(&self) -> usize {
         match self {
@@ -65,19 +27,27 @@ impl BitsQueryable for MemoryAddress {
     }
 }
 
+macro_rules! impl_bits_queryable_for_integers {
+    ($($t:ty),+) => {
+        $(
+            impl BitsQueryable for $t {
+                fn num_bits(&self) -> usize {
+                    get_msb(*self as u128)
+                }
+            }
+        )+
+    };
+}
+
+impl_bits_queryable_for_integers!(u8, u16, u32, u64, u128, usize);
+
 pub fn bits_needed_for<T: BitsQueryable>(val: &T) -> usize {
-    let num_bits = val.num_bits();
-    if num_bits <= 8 {
-        8
-    } else if num_bits <= 16 {
-        16
-    } else if num_bits <= 32 {
-        32
-    } else if num_bits <= 64 {
-        64
-    } else if num_bits <= 128 {
-        128
-    } else {
-        254
+    match val.num_bits() {
+        0..=8 => 8,
+        9..=16 => 16,
+        17..=32 => 32,
+        33..=64 => 64,
+        65..=128 => 128,
+        _ => 254,
     }
 }
